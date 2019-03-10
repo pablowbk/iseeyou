7fc37cbbf7ed5a77fab4173dbe15c04f55f05638
pablowbk <pablo.weisbek@gmail.com>
2019-03-10 10:19:27 -0300

    project initial setup

---
+++ src/comps/nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav className="Nav" style={
+      {display: 'flex', justifyContent: 'flex-end', padding: '0px 20px'}
+    }>
+      <p>Sign Out</p>
+    </nav>
+  );
+}
+
+export default Nav;
